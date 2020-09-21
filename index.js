1d0df4c6b7f0a24458d9624a24a6cb09a1a75a2e
illialaz <illia.laziuk2@gmail.com>
2020-09-21 22:33:07 +0300

    Create RSS reader desctop app using Electron

---
+++ index.js
@@ -0,0 +1,36 @@
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+const fs = require('fs')
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    },
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  win.webContents.on('did-finish-load', function () {
+    fs.readFile(__dirname + '/tailwind-ui.min.css', 'utf-8', function (
+      error,
+      data
+    ) {
+      console.log('ok')
+      if (!error) {
+        var formatedData = data.replace(/\s{2,10}/g, ' ').trim()
+        win.webContents.insertCSS(formatedData)
+      }
+    })
+  })
+}
+
+app.whenReady().then(createWindow)
